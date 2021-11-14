11c8bd333aba16d291dbb1590db24d8f9921d1ef
Nguyễn Long Vũ <76510051+LongVux@users.noreply.github.com>
2021-11-14 15:21:51 +0700

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4994cfde8da35248d6a226c611ab2dfd'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0d376445-c280-41ee-b31f-69c7de11cdb6'
+};
